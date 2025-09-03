53b869a409480393d3b4c3e3472a0ceb0a12c3b2
honu5 <liranso@gmail.com>
2025-09-03 08:59:15 -0700

    The sign up page is completed and additional features are added on the landing page

---
+++ public/src/script.js
@@ -0,0 +1,9 @@
+'use strict';
+const modeToggle = document.getElementById("modeToggle");
+const body = document.body;
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+  body.classList.toggle("dark-mode");
+  modeToggle.textContent = body.classList.contains("light-mode") ? "🌙" : "☀️";
+});
