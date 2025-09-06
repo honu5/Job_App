2afe463849461128ff95311bd31f9fd502f87dfe
LombameLemma <lombamelemma@gmail.com>
2025-09-06 00:53:05 -0700

    signup.html moved to views folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Database connection pool
 const pool = new Pool({
     user: 'your_username',
